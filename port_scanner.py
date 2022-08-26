a0eeae460eec7906e812fdf19341af06881507dd
Matheus Heidemann <heidemann.principal@gmail.com>
2022-08-25 21:49:01 -0300

    Fazendo

--- port_scanner.py
+++ port_scanner.py
@@ -1,17 +1,42 @@
+# TODO
+# Printar as portas abertas por padrão
+# Opção para printar portas fechadas
+# Pegar os banners
+# Usar regex para detectar o OS através dos banners
+# Opções de scan personalizadas (portas 1-1024, 1-65565, as 10 mais comuns)
+# 192.168.220.130
+
 from socket import *
 import time
-startTime = time.time()
+import argparse
 
-if __name__ == '__main__':
-    target = input('Enter the host to be scanned: ')
-    t_IP = gethostbyname(target)
-    print('Starting scan on host: ', t_IP)
-    for i in range(4440, 4450):
-        print(i)
-        s = socket(AF_INET, SOCK_STREAM)
 
-        conn = s.connect_ex((t_IP, i))
-        if (conn == 0):
-            print('Port %d: OPEN' % (i,))
-        s.close()
-print('Time taken:', time.time() - startTime)
+class PortScanner:
+    def __init__(self, target):
+        self.target = target
+
+    def scanPort(self, port):
+        socket_obj = socket(AF_INET, SOCK_STREAM)
+        socket_obj.settimeout(1)
+        result = socket_obj.connect_ex((self.target, port))
+        if (result == 0):
+            try:
+                banner = socket_obj.recv(1024).decode()
+                print(f"Port {port}")
+                print("Status: open")
+                print("Banner:")
+            except Exception as e:
+                print(e)
+                pass
+        socket_obj.close()
+
+    def scan(self):
+        for port in range(1, 1024):
+            self.scanPort(port)
+
+
+if __name__ == "__main__":
+    ps = PortScanner(
+        target="192.168.220.130"
+    )
+    ps.scan()
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,17 +1,58 @@
+# TODO
+# Printar as portas abertas por padrão
+# Opção para printar portas fechadas
+# Pegar os banners
+# Usar regex para detectar o OS através dos banners
+# Opções de scan personalizadas (portas 1-1024, 1-65565, as 10 mais comuns)
+
 from socket import *
 import time
-startTime = time.time()
+import argparse
 
-if __name__ == '__main__':
-    target = input('Enter the host to be scanned: ')
-    t_IP = gethostbyname(target)
-    print('Starting scan on host: ', t_IP)
-    for i in range(4440, 4450):
-        print(i)
+if __name__ == 'pas':
+    for i in range(79, 65535):
         s = socket(AF_INET, SOCK_STREAM)
 
         conn = s.connect_ex((t_IP, i))
+        print(conn)
+        input()
         if (conn == 0):
             print('Port %d: OPEN' % (i,))
         s.close()
-print('Time taken:', time.time() - startTime)
+
+# 192.168.220.130
+
+
+class PortScanner:
+    def __init__(self, target):
+        self.target = target
+
+    def scanPort(self, port):
+        socket_obj = socket(AF_INET, SOCK_STREAM)
+        socket_obj.settimeout(1)
+        result = socket_obj.connect_ex((self.target, port))
+        if (result == 0):
+            try:
+                socket.gethostbyaddr(self.target)[0]
+                service = socket.getservbyport(port)
+                banner = socket_obj.recv(1024).decode()
+                print(f"Port {port}")
+                print("Status: open")
+                print("Banner:")
+                print(banner)
+                print(service)
+            except Exception as e:
+                print(e)
+                pass
+        socket_obj.close()
+
+    def scan(self):
+        for port in range(1, 1024):
+            self.scanPort(port)
+
+
+if __name__ == "__main__":
+    ps = PortScanner(
+        target="192.168.220.130"
+    )
+    ps.scan()
